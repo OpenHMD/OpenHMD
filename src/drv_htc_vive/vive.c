9a7729689383937237782c2c428ba9381241344f
TheOnlyJoey <joeyferweda@gmail.com>
2018-03-30 18:40:53 +0200

    Added find_usb_end_point to Vive due to Windows changing the order of interfaces from system to system

--- src/drv_htc_vive/vive.c
+++ src/drv_htc_vive/vive.c
@@ -256,7 +256,7 @@ static hid_device* open_device_idx(int manufacturer, int product, int iface, int
 	while (cur_dev) {
 		printf("%04x:%04x %s\n", manufacturer, product, cur_dev->path);
 
-		if(idx == device_index && iface == iface_cur){
+		if(findEndPoint(cur_dev->path, device_index) > 0 && iface == iface_cur) {
 			ret = hid_open_path(cur_dev->path);
 			printf("opening\n");
 		}
