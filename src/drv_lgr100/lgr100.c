58c8ca5afe2b9fdd09e75ad498616f0df2116320
TheOnlyJoey <joeyferweda@gmail.com>
2018-12-05 16:31:30 +0100

    First functional version with IMU data and button states, no fusion being done yet

---
+++ src/drv_lgr100/lgr100.c
@@ -0,0 +1,223 @@
+// Copyright 2018, Joey Ferwerda.
+// SPDX-License-Identifier: BSL-1.0
+/*
+ * OpenHMD - Free and Open Source API and drivers for immersive technology.
+ */
+
+/* LG R100 (360VR) Driver */
+
+#include <string.h>
+#include <hidapi.h>
+#include "../openhmdi.h"
+
+#include "lgr100.h"
+
+typedef struct {
+	ohmd_device base;
+	hid_device* handle;
+	int id;
+
+	int controller_values[2];
+	lgr100_sensor_sample sample;
+	fusion sensor_fusion;
+	vec3f raw_accel, raw_gyro;
+} lgr100_priv;
+
+static lgr100_priv* lgr100_priv_get(ohmd_device* device)
+{
+	return (lgr100_priv*)device;
+}
+
+static void handle_tracker_sensor_msg(lgr100_priv* priv, unsigned char* buffer, int size)
+{
+	decode_lgr100_imu_msg(&priv->sample, buffer, size);
+}
+
+static void update_device(ohmd_device* device)
+{
+	lgr100_priv* priv = lgr100_priv_get(device);
+	unsigned char buffer[FEATURE_BUFFER_SIZE];
+
+	// Read all the messages from the device.
+	while(true){
+		int size = hid_read(priv->handle, buffer, FEATURE_BUFFER_SIZE);
+		if(size < 0){
+			LOGE("error reading from device");
+			return;
+		} else if(size == 0) {
+			return; // No more messages, return.
+		}
+
+		if(buffer[0] == 0) {
+		}
+		// Type 5 seems to contain IMU information
+		if(buffer[0] == 5) {
+			handle_tracker_sensor_msg(priv, buffer, size);
+		}
+		else if(buffer[0] == 2){
+			//button 'OK' is buffer[1] state 01 and 04
+			//button '<-' is buffer[1] state 02 and 03
+			LOGE("Looking at: %u", buffer[1]);
+			if (buffer[1] == 1)
+				priv->controller_values[0] = 1;
+			else if (buffer[1] == 2)
+				priv->controller_values[1] = 1;
+			else if (buffer[1] == 3)
+				priv->controller_values[1] = 0;
+			else if (buffer[1] == 4)
+				priv->controller_values[0] = 0;
+		}else{
+			//LOGE("unknown message type: %u", buffer[0]);
+		}
+	}
+}
+
+static int getf(ohmd_device* device, ohmd_float_value type, float* out)
+{
+	lgr100_priv* priv = (lgr100_priv*)device;
+
+	switch(type){
+	case OHMD_ROTATION_QUAT: {
+			*(quatf*)out = priv->sensor_fusion.orient;
+			break;
+		}
+
+	case OHMD_POSITION_VECTOR:
+		out[0] = out[1] = out[2] = 0;
+		break;
+
+	case OHMD_DISTORTION_K:
+		// TODO this should be set to the equivalent of no distortion
+		memset(out, 0, sizeof(float) * 6);
+		break;
+	
+	case OHMD_CONTROLS_STATE:
+		for (int i = 0; i < 2; i++){
+			out[i] = priv->controller_values[i];
+		}
+		break;
+
+	default:
+		ohmd_set_error(priv->base.ctx, "invalid type given to getf (%ud)", type);
+		return OHMD_S_INVALID_PARAMETER;
+		break;
+	}
+
+	return OHMD_S_OK;
+}
+
+static void close_device(ohmd_device* device)
+{
+	LOGD("closing lgr100 device");
+	free(device);
+}
+
+static ohmd_device* open_device(ohmd_driver* driver, ohmd_device_desc* desc)
+{
+	lgr100_priv* priv = ohmd_alloc(driver->ctx, sizeof(lgr100_priv));
+	if(!priv)
+		return NULL;
+	
+	priv->id = desc->id;
+	priv->base.ctx = driver->ctx;
+
+	// Open the HID device
+	priv->handle = hid_open_path(desc->path);
+
+	if(!priv->handle) {
+		goto cleanup;
+	}
+
+	//Start the headset with the 'vr start app' command
+	hid_write(priv->handle, start_device, sizeof(start_device));
+
+	//Keep Alive
+	hid_write(priv->handle, keep_alive, sizeof(keep_alive));
+
+	hid_write(priv->handle, start_accel, sizeof(start_accel));
+	hid_write(priv->handle, start_gyro, sizeof(start_gyro));
+
+	// Set default device properties
+	ohmd_set_default_device_properties(&priv->base.properties);
+
+	// Set device properties (currently just aproximations)
+	priv->base.properties.hsize = 0.149760f;
+	priv->base.properties.vsize = 0.093600f;
+	priv->base.properties.hres = 1440;
+	priv->base.properties.vres = 960;
+	priv->base.properties.lens_sep = 0.063500f;
+	priv->base.properties.lens_vpos = 0.046800f;
+	priv->base.properties.fov = DEG_TO_RAD(80.0f); //based on website information, probably not perfect
+	priv->base.properties.ratio = (1440.0f / 960.0f) / 2.0f;
+	
+	// Some buttons and axes
+	priv->base.properties.control_count = 2;
+	priv->base.properties.controls_hints[0] = OHMD_BUTTON_A;
+	priv->base.properties.controls_hints[1] = OHMD_BUTTON_B;
+	priv->base.properties.controls_types[0] = OHMD_DIGITAL;
+	priv->base.properties.controls_types[1] = OHMD_DIGITAL;
+
+	// calculate projection eye projection matrices from the device properties
+	ohmd_calc_default_proj_matrices(&priv->base.properties);
+
+	// set up device callbacks
+	priv->base.update = update_device;
+	priv->base.close = close_device;
+	priv->base.getf = getf;
+
+	// initialize sensor fusion
+	ofusion_init(&priv->sensor_fusion);
+
+	return (ohmd_device*)priv;
+
+cleanup:
+	if(priv)
+		free(priv);
+
+	return NULL;
+}
+
+static void get_device_list(ohmd_driver* driver, ohmd_device_list* list)
+{
+	int id = 0;
+	struct hid_device_info* cur_dev = hid_enumerate(0x1004, 0x6374);
+	if(cur_dev == NULL)
+		return;
+	ohmd_device_desc* desc;
+
+	// HMD
+	desc = &list->devices[list->num_devices++];
+
+	strcpy(desc->driver, "OpenHMD LG R100 (360VR) Driver");
+	strcpy(desc->vendor, "LG");
+	strcpy(desc->product, "LGR100");
+
+	strcpy(desc->path, cur_dev->path);
+
+	desc->driver_ptr = driver;
+
+	desc->device_flags = OHMD_DEVICE_FLAGS_ROTATIONAL_TRACKING;
+	desc->device_class = OHMD_DEVICE_CLASS_HMD;
+
+	desc->id = id++;
+	hid_free_enumeration(cur_dev);
+}
+
+static void destroy_driver(ohmd_driver* drv)
+{
+	LOGD("shutting down lgr100 driver");
+	free(drv);
+}
+
+ohmd_driver* ohmd_create_lgr100_drv(ohmd_context* ctx)
+{
+	ohmd_driver* drv = ohmd_alloc(ctx, sizeof(ohmd_driver));
+	if(!drv)
+		return NULL;
+
+	drv->get_device_list = get_device_list;
+	drv->open_device = open_device;
+	drv->destroy = destroy_driver;
+
+	return drv;
+}
---
+++ src/drv_lgr100/packet.c
@@ -0,0 +1,34 @@
+// Copyright 2018, Joey Ferwerda.
+// SPDX-License-Identifier: BSL-1.0
+/*
+ * OpenHMD - Free and Open Source API and drivers for immersive technology.
+ */
+
+/* LG R100 (360VR) Driver */
+
+
+#include <stdio.h>
+#include "lgr100.h"
+
+#define SKIP8 (buffer++)
+#define SKIP_CMD (buffer++)
+#define READ8 *(buffer++);
+#define READ16 *buffer | (*(buffer + 1) << 8); buffer += 2;
+#define READ32 *buffer | (*(buffer + 1) << 8) | (*(buffer + 2) << 16) | (*(buffer + 3) << 24); buffer += 4;
+#define READFLOAT ((float)(*buffer)); buffer += 4;
+#define READFIXED (float)(*buffer | (*(buffer + 1) << 8) | (*(buffer + 2) << 16) | (*(buffer + 3) << 24)) / 1000000.0f; buffer += 4;
+
+#define WRITE8(_val) *(buffer++) = (_val);
+#define WRITE16(_val) WRITE8((_val) & 0xff); WRITE8(((_val) >> 8) & 0xff);
+#define WRITE32(_val) WRITE16((_val) & 0xffff) *buffer; WRITE16(((_val) >> 16) & 0xffff);
+
+
+bool decode_lgr100_imu_msg(lgr100_sensor_sample* smp, const unsigned char* buffer, int size)
+{
+	if(!size == 32){
+		LOGE("invalid packet size (expected 32 but got %d)", size);
+		return false;
+	}
+
+	return true;
+}
