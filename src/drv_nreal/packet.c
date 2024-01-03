05b50edd44a3a4de40ae180e987f2745026a7f61
ila <ila.embsys@gmail.com>
2024-01-04 03:28:35 +0400

    nreal: remove [[maybe unused]]

--- src/drv_nreal/packet.c
+++ src/drv_nreal/packet.c
@@ -97,7 +97,7 @@ read_u16(const uint8_t **buffer, uint16_t *out_value)
 	*buffer += 2;
 }
 
-[[maybe_unused]] static inline void
+static inline void
 read_u32(const uint8_t **buffer, uint32_t *out_value)
 {
 	*out_value = (*(*buffer + 0) << 0u) |  // Byte 0
